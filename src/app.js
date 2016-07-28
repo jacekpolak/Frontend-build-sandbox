c5b1eabbd35cf23f3d0ca33b248ad3d80c80a870
Oleksii An <Oleksii_An@epam.com>
2016-07-28 14:40:57 +0300

    Added comments.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+// NOTE: Such files could be populated to take each page's bundle
 import start from 'helpers/start.js';
 import Factory from 'helpers/Factory.js';
 
 import 'components/component.js';
 import './app.scss';
 
+// Lights, camera, action!
 start(Factory);
